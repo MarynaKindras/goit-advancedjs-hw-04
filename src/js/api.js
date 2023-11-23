5330c0ce927a096ecad4da497a66019de5845f4a
MarynaKindras <maryna.kindras@gmail.com>
2023-11-23 14:50:06 +0200

    Change 2

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
-const PIXABAY_API_KEY = '38153800-29c65605892730b6f027a7070';
+const PIXABAY_API_KEY = '40851648-2156548c3e3c31aec5105189d';
 import axios from 'axios';
 
 export async function fetchImages(query, pageNumber) {
